362b9a7c3aa9950128f8bfb8f405cd064ad7b700
praveenkumar <praveenpkbadiger@gmail.com>
2022-07-20 08:45:09 +0530

    [praveen] adding files

---
+++ Trailingfactorial.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+Step 1  import java.io.*;
+step 2 create static method findtrailingzeros
+step 3  if(n<0) return -1
+step 3 inrt count =0;
+step 4  for(int i=0; n/i>=1; i *=5)
+step 5 count += n/i;
+step 6 return count;
+step 7 int n=20;
+step 8 print n and static method
+step 9 output
+
